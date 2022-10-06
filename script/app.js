aefbf55e3cb85b3fadbe47275fd51f3bb38a93b7
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2022-10-06 15:52:40 +0200

    Password Toggle

--- script/app.js
+++ script/app.js
@@ -1,13 +1,29 @@
+const passwordOptions = ['password', 'text'];
+
 function handleFloatingLabel() {
    
 }
 
+/* function handlePasswordSwitcher() {
+  var x = document.getElementById('password');
+  if (x.type === 'text') {
+    x.type = 'password';
+  } else {
+    x.type = 'text';
+  }
+} */
+
 function handlePasswordSwitcher() {
-    
+    var passwordInput = document.getElementById('password');
+    var passwordToggle = document.getElementById('togglePassword');
+    passwordToggle.addEventListener('change', function () {
+        passwordInput.type = passwordOptions[+this.checked];
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
     handleFloatingLabel();
-    handlePasswordSwitcher();
+    //handlePasswordSwitcher();
 });
+
